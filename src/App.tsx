6b0755a53c19b337d3b043291595a9f6f75b3cc2
RPE8 <vitali.shuman96@gmail.com>
2022-11-07 15:07:32 +0300

    Create TopTable and BottomTable components. Pass them to Layout comp.

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import {layout as Layout} from './components/layouts/main/layout';
 import {header as Header} from "./components/header/header";
+import {bottomTable as BottomTable} from "./components/bottomTable/bottomTable";
+import {topTable as TopTable} from "./components/topTable/topTable";
 import './App.css';
 
 function App() {
   return (
 	  <div className="App">
-		<Layout header={<Header />}>
+		<Layout header={<Header />} topContent={<TopTable />} bottomContent={<BottomTable />}>
 		</Layout>
     </div>
   );
--- src/components/layouts/main/layout.tsx
+++ src/components/layouts/main/layout.tsx
@@ -5,15 +5,15 @@ type ReactElementOrNull = ReactElement | null;
 
 type LayoutProps = {
 	header: ReactElementOrNull;
-	// topContent: ReactElementOrNull;
-	// bottomContent: ReactElementOrNull;
+	topContent: ReactElementOrNull;
+	bottomContent: ReactElementOrNull;
 	children: never[];
 };
 
-export const layout = ({header} : LayoutProps) => {
+export const layout = ({header, topContent, bottomContent} : LayoutProps) => {
 	return <div>
 		<div className="header-container">{header}</div>
-		{/* <div className="topContent-container">{topContent}</div>
-		<div className="bottomContent-container">{bottomContent}</div> */}
+		<div className="topContent-container">{topContent}</div>
+		<div className="bottomContent-container">{bottomContent}</div>
 	</div>
 }
