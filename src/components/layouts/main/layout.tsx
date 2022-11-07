142df7dce0ca35eb0d1213bcb2f6168eea7de7cf
RPE8 <vitali.shuman96@gmail.com>
2022-11-07 14:58:43 +0300

    Add initial Layout component

---
+++ src/components/layouts/main/layout.tsx
@@ -0,0 +1,18 @@
+import React, { FunctionComponent, ReactElement } from 'react';
+
+type ReactElementOrNull = ReactElement | null;
+
+type LayoutProps = {
+	header: ReactElementOrNull;
+	// topContent: ReactElementOrNull;
+	// bottomContent: ReactElementOrNull;
+	children: never[];
+};
+
+export const layout = ({header} : LayoutProps) => {
+	return <div>
+		<div className="header-container">{header}</div>
+		{/* <div className="topContent-container">{topContent}</div>
+		<div className="bottomContent-container">{bottomContent}</div> */}
+	</div>
+}
