ead80ccbc546b658c89ddf47815e25a893a2960a
RPE8 <vitali.shuman96@gmail.com>
2022-11-08 14:48:03 +0300

    Fix return statement style

--- src/components/layouts/main/layout.tsx
+++ src/components/layouts/main/layout.tsx
@@ -12,9 +12,11 @@ type LayoutProps = {
 };
 
 export const layout = ({header, topContent, bottomContent} : LayoutProps) => {
-	return <div className="container">
-		<div className="container__header">{header}</div>
-		<div className="container__topContent">{topContent}</div>
-		<div className="container__bottomContainer">{bottomContent}</div>
-	</div>
+	return (
+		<div className="container">
+			<div className="container__header">{header}</div>
+			<div className="container__topContent">{topContent}</div>
+			<div className="container__bottomContainer">{bottomContent}</div>
+		</div>
+	);
 }
