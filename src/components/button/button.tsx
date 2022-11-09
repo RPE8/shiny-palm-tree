d5019d783d5ace8478747654ddf139f691d09381
RPE8 <vitali.shuman96@gmail.com>
2022-11-09 19:59:02 +0300

    Add initial Button component

---
+++ src/components/button/button.tsx
@@ -0,0 +1,30 @@
+import "./button.scss";
+type props = {
+	children: string;
+	variant: "text" | "outlined" | "contained";
+	disabled?: boolean;
+	onClick?: Function;
+};
+export const Button = ({
+	variant,
+	onClick,
+	children,
+	disabled = false,
+}: props) => {
+	const classes = `button button-${variant}${
+		disabled ? " button--disabled" : ""
+	}`;
+	return (
+		<button
+			className={classes}
+			onClick={() => {
+				console.log("button clicked");
+				if (disabled) return;
+				console.log("button is active");
+				onClick?.();
+			}}
+		>
+			{children}
+		</button>
+	);
+};
