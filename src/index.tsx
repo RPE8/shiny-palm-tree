735d650d64fecfa340e59d1273bf3956e0f2bf16
RPE8 <vitali.shuman96@gmail.com>
2022-11-09 15:27:03 +0300

    Create initial mock logic and send GET request on App component mounting

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,26 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(
-	document.getElementById("root") as HTMLElement
-);
-root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-);
+function prepareMock() {
+	if (process.env.NODE_ENV === "development") {
+		const { worker } = require("./mocks/browser");
+		return worker.start();
+	}
+	return Promise.resolve();
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+prepareMock().then(() => {
+	const root = ReactDOM.createRoot(
+		document.getElementById("root") as HTMLElement
+	);
+	root.render(
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	);
+	// If you want to start measuring performance in your app, pass a function
+	// to log results (for example: reportWebVitals(console.log))
+	// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+	reportWebVitals();
+});
+
