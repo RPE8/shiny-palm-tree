e6f1becc10bc98aa62da6ac9ff49b147cc9febbb
unknown <YUlasenka@gomel.iba.by>
2022-11-14 13:52:57 +0300

    Declare source link in comment

--- src/components/iconButton/iconButton.js
+++ src/components/iconButton/iconButton.js
@@ -2,6 +2,8 @@ import "./iconButton.scss";
 import { Button } from "../button/button";
 import * as Icons from "react-icons/fa"
 
+//see logos from https://react-icons.github.io/react-icons/icons?name=fa
+
 type ValidVariants = "text" | "outlined" | "contained";
 type ValidColors = "success" | "error";
 type ValidSizes = "small" | "medium" | "large";
